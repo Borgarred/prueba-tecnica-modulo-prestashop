f6cb97c616bcb2e73b5d5d0d25f3caccbab3bba7
Borgarred <borjagarrared@gmail.com>
2025-08-30 21:18:09 +0200

    eliminar controller payment

--- prestashop-src/modules/pago_tarjeta_prueba/controllers/front/payment.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Mi_TarjetaPaymentModuleFrontController extends ModuleFrontController
-{
-public function initContent()
-{
-parent::initContent();
-$this->setTemplate('module:mi_tarjeta/views/templates/hook/payment_form.tpl');
-}
-}
